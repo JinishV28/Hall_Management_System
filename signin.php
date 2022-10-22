50f56a98b54bbd1515aecbb731cbfb3173bb8fd2
JinishV28 <jinishvaraiya@gmail.com>
2022-10-22 14:24:21 +0530

    Fourth Commit
    Student Dashboard with registration form. Little changes in CSS as well

--- signin.php
+++ signin.php
@@ -3,12 +3,22 @@
 
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $sql = "Select * from signup_info where(email='$email' and password='$password')";
+    $sql = "Select * from signup_info where(email='$email')";
     $result = $conn->query($sql);
     $count = mysqli_num_rows($result);
     if($count==1)
     {
-        echo "<script> window.location.assign('student.html'); </script>";
+        $sql = "Select * from signup_info where(password='$password')";
+        $result = $conn->query($sql);
+        $count = mysqli_num_rows($result);
+        if($count==1)
+        {
+            echo "<script>window.location.assign('student.html');</script>";
+        }
+        else
+        {
+            echo "Wrong Password";
+        }
     }
     else
     {
